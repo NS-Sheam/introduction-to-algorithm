7f5d407e6fdd0bbe684a6eb61c97e39fe07077c4
NS-Sheam <123sheamfeni@gmail.com>
2025-12-10 16:35:25 +0600

    add: doremy paint

--- week-5-bonus-week/road-to-xpsc/A_Doremy_s_Paint_3.cpp
+++ week-5-bonus-week/road-to-xpsc/A_Doremy_s_Paint_3.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-map<int, int> freq;
 int main()
 {
     int t;
@@ -10,24 +9,45 @@ int main()
         int n;
         cin >> n;
         vector<int> ar(n);
+        map<int, int> mp;
         for (int i = 0; i < n; i++)
         {
             int a;
             cin >> a;
-            freq[a]++;
-            if (freq[a] > 2)
-            {
-                cout << "NO" << endl;
-                break;
-            }
+            mp[a]++;
             ar[i] = a;
-            // cout << freq[a] << " ";
+            // cout << mp[a] << " ";
         }
         // for (int i = 0; i < n; i++)
         // {
         //     cout << ar[i] << " ";
         // }
-        cout << endl;
+        // for (auto it = mp.begin(); it != mp.end(); ++it)
+        // {
+        //     cout << "Key: " << it->first << ", Value: " << it->second << endl;
+        // }
+        if (mp.size() > 2)
+        {
+            cout << "No" << endl;
+        }
+        else
+        {
+
+            if (mp.size() == 2)
+            {
+                int diff = 0;
+                for (pair<int, int> p : mp)
+                {
+                    diff = abs(diff - p.second);
+                }
+                if (diff > 1)
+                    cout << "No" << endl;
+                else
+                    cout << "Yes" << endl;
+            }
+            else
+                cout << "Yes" << endl;
+        }
     }
 
     return 0;
--- week-5-bonus-week/road-to-xpsc/tempCodeRunnerFile.cpp
+++ week-5-bonus-week/road-to-xpsc/tempCodeRunnerFile.cpp
@@ -1,1 +1,5 @@
-in()
+ // for (auto it = mp.begin(); it != mp.end(); ++it)
+        // {
+        //     cout << "Key: " << it->first << ", Value: " << it->second << endl;
+        // }
+        // cout << mp.siz
