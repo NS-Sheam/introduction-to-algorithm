69367cd643fb282383f156a2228ca54a2d5a785f
NS-Sheam <123sheamfeni@gmail.com>
2025-12-10 15:38:27 +0600

    add: a simple palindrome complete

--- week-5-bonus-week/road-to-xpsc/A_Simple_Palindrome.cpp
+++ week-5-bonus-week/road-to-xpsc/A_Simple_Palindrome.cpp
@@ -3,5 +3,24 @@ using namespace std;
 int main()
 {
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<char> v = {'a', 'e', 'i', 'o', 'u'};
+        string s = "";
+        int i = 0;
+        while (n--)
+        {
+            s += v[i];
+            i++;
+            if (i == 5)
+                i = 0;
+        }
+        sort(s.begin(), s.end());
+        cout << s << endl;
+    }
     return 0;
 }
