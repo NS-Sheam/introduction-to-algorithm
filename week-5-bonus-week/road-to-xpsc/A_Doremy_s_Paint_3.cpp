80a979158f7d4076f4c43a2ef3f25102b4e1f69b
NS-Sheam <123sheamfeni@gmail.com>
2025-12-10 16:14:38 +0600

    adding: doremy paint

--- week-5-bonus-week/road-to-xpsc/A_Doremy_s_Paint_3.cpp
+++ week-5-bonus-week/road-to-xpsc/A_Doremy_s_Paint_3.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+map<int, int> freq;
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> ar(n);
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            freq[a]++;
+            if (freq[a] > 2)
+            {
+                cout << "NO" << endl;
+                break;
+            }
+            ar[i] = a;
+            // cout << freq[a] << " ";
+        }
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cout << ar[i] << " ";
+        // }
+        cout << endl;
+    }
 
     return 0;
 }
