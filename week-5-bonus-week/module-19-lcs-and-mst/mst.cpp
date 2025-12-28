7209b4db91abe7ffdad0e099b3b18ed05a98bfd5
NS-Sheam <123sheamfeni@gmail.com>
2025-12-29 00:31:33 +0600

    add: kruskal Algorithm

--- week-5-bonus-week/module-19-lcs-and-mst/mst.cpp
+++ week-5-bonus-week/module-19-lcs-and-mst/mst.cpp
@@ -1,5 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int par[1005];
+int group_size[1005];
+
+int find(int node)
+{
+    if (par[node] == -1)
+        return node;
+    int leader = find(par[node]);
+    par[node] = leader;
+    return leader;
+}
+
+void dsu_union(int node1, int node2)
+{
+    int leader1 = find(node1);
+    int leader2 = find(node2);
+    if (group_size[leader1] >= group_size[leader2])
+    {
+        par[leader2] = leader1;
+        group_size[leader1] += group_size[leader2];
+    }
+    else
+    {
+        par[leader1] = leader2;
+        group_size[leader2] += group_size[leader1];
+    }
+}
+
 class Edge
 {
 public:
@@ -29,9 +58,19 @@ int main()
         edge_list.push_back(Edge(a, b, c));
     }
     sort(edge_list.begin(), edge_list.end(), cmp);
+    memset(par, -1, sizeof(par));
+    memset(group_size, -1, sizeof(group_size));
+    int totalCost = 0;
     for (auto edge : edge_list)
     {
-        cout << edge.a << " " << edge.b << " " << edge.c << endl;
+        int parA = find(edge.a);
+        int parB = find(edge.b);
+        if (parA != parB)
+        {
+            dsu_union(edge.a, edge.b);
+            totalCost += edge.c;
+        }
     }
+    cout << "Total Cost: " << totalCost;
     return 0;
 }
